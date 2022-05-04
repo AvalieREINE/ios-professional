efa342d75ff6dcfac8f4d0920bd22e02eeb4e04c
Avalie Zhang <avaliezhnag>
2022-05-04 21:40:14 +1200

    add networking and unit tests

---
+++ Bankey/Bankey/Utils/Date+Utils.swift
@@ -0,0 +1,21 @@
+//
+//  Date+Utils.swift
+//  Bankey
+//
+//  Created by Soeur Zhang on 22/04/22.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
